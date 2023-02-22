2da1d41112d5e96c9914484cef122bb337b7639f
swheel <swheel33@gmail.com>
2023-02-22 15:23:46 -0500

    feat: add copy button for path

--- src/components/StopSelect.tsx
+++ src/components/StopSelect.tsx
@@ -1,5 +1,16 @@
-import { Button, Radio, Select, Stack, Text } from "@mantine/core"
+import {
+  ActionIcon,
+  CopyButton,
+  Group,
+  Radio,
+  Select,
+  Stack,
+  Text,
+  Tooltip,
+} from "@mantine/core"
 import { useForm } from "@mantine/form"
+import { IconCheck, IconCopy } from "@tabler/icons-react"
+
 import { validTrains } from "lib/contants"
 import { formattedStops } from "lib/helpers"
 
@@ -31,7 +42,26 @@ export const StopSelect = () => {
           <Radio value="north" label="Uptown" />
           <Radio value="south" label="Downtown" />
         </Radio.Group>
-        <Text weight='bold' color='green'>Path: /api/{form.values.train}/{form.values.stop}/{form.values.direction}</Text>
+        <Group>
+          <Text weight="bold" color="green">
+            Path: /api/{form.values.train}/{form.values.stop}/
+            {form.values.direction}
+          </Text>
+          <CopyButton value={`/api/${form.values.train}/${form.values.stop}/
+            ${form.values.direction}`} timeout={2000}>
+            {({ copied, copy }) => (
+              <Tooltip
+                label={copied ? "Copied" : "Copy"}
+                withArrow
+                position="right"
+              >
+                <ActionIcon color={copied ? "teal" : "gray"} onClick={copy}>
+                  {copied ? <IconCheck size={16} /> : <IconCopy size={16} />}
+                </ActionIcon>
+              </Tooltip>
+            )}
+          </CopyButton>
+        </Group>
       </Stack>
     </form>
   )
